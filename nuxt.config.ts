cd5452aa47864e2edd301b81d85cac4d3a1f51b6
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-10-30 12:40:21 +0700

    nuxt ui module added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,7 @@ export default defineNuxtConfig({
               config: {},
               injectPosition: 'first',
               viewer: true,
-            }
+            },
+  modules: ['@nuxt/ui']
 })
 
