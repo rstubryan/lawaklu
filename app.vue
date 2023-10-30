92a8eee18516e373217dbc967e1a21855a94e145
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-10-30 17:07:49 +0700

    template set nuxt

--- app.vue
+++ app.vue
@@ -1,11 +1,3 @@
 <template>
-  <body class="bg-teal-100">
-    <div class="bg-teal-600">
-      <section class="container mx-auto">
-        <div class="py-4">
-          <p class="text-white text-4xl text-center">Hello World</p>
-        </div>
-      </section>
-    </div>
-  </body>
+  <NuxtPage />
 </template>
