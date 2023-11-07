8f4091e81da9925cca270db13febeb862582e6d0
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-11-07 22:20:14 +0700

    refactor syntax nested route landing page

--- pages/index.vue
+++ pages/index.vue
@@ -2,29 +2,13 @@
   <div>
     <AppHeader />
     <div class="mt-32">
-      <HeroSection />
+      <IndexpHeroSection />
     </div>
     <div class="mt-16">
-      <LogoPadding />
+      <IndexpLogoPadding />
     </div>
     <div class="mt-16">
-      <SpacingBar />
+      <IndexpSpacingBar />
     </div>
   </div>
 </template>
-
-<script>
-import AppHeader from "@/components/AppHeader.vue";
-import HeroSection from "@/components/landing_page/HeroSection.vue";
-import LogoPadding from "@/components/landing_page/LogoPadding.vue";
-import SpacingBar from "@/components/landing_page/SpacingBar.vue";
-
-export default {
-  components: {
-    AppHeader,
-    HeroSection,
-    LogoPadding,
-    SpacingBar,
-  },
-};
-</script>
