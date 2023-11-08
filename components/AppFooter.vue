6cbcc2762486e0e71ca4944d1b76ac3a3e3faf45
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-11-08 14:40:34 +0700

    update space and footer set, wip number-r spacing

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,13 +1,37 @@
 <template>
-  <div class="py-4 bg-white font-raleway">
+  <div class="py-20 font-raleway">
     <div class="container mx-auto">
-      <p class="text-base text-center text-black">Copyright Lawaklu</p>
+      <div class="flex flex-row justify-between">
+        <div class="justify-start text-green-950">
+          <h2 class="font-semibold text-3xl">Lawaklu</h2>
+          <p class="w-96 text-base mt-4">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Natus
+            minus iusto tempora, quis aut ratione aspernatur aliquid non
+            voluptates vitae! Asperiores soluta odio voluptatem voluptatum,
+            dolorem quasi blanditiis minima! Quod.
+          </p>
+        </div>
+        <div class="flex flex-row gap-32 text-green-950 justify-end mr-6">
+          <div class="grid">
+            <h3 class="text-lg font-bold">Social Media</h3>
+            <NuxtLink to="/about">Facebook</NuxtLink>
+            <NuxtLink to="/about">Facebook</NuxtLink>
+            <NuxtLink to="/about">Facebook</NuxtLink>
+          </div>
+          <div class="grid">
+            <h3 class="text-lg font-bold">Social Media</h3>
+            <NuxtLink to="/about">Facebook</NuxtLink>
+            <NuxtLink to="/about">Facebook</NuxtLink>
+            <NuxtLink to="/about">Facebook</NuxtLink>
+          </div>
+          <div class="grid">
+            <h3 class="text-lg font-bold">Social Media</h3>
+            <NuxtLink to="/about">Facebook</NuxtLink>
+            <NuxtLink to="/about">Facebook</NuxtLink>
+            <NuxtLink to="/about">Facebook</NuxtLink>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
-
-<style>
-.font-raleway {
-  font-family: "Raleway", sans-serif;
-}
-</style>
