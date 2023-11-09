e2ccc271bc6e8d5e97699c58ecf988ed73fe7bb0
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-11-09 14:30:03 +0700

    wip sidebar on forum's menu

---
+++ components/forum/SidebarSection.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class>
+    <div class="w-96 h-96">
+      
+    </div>
+  </div>
+</template>
--- layouts/forum-menu.vue
+++ layouts/forum-menu.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <AppHeader />
-    <slot />
+    <ForumSidebarSection />
     <AppFooter />
   </div>
 </template>
