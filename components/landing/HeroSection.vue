319133e866fa2d4f3d9d34c638da9b688f60a7b8
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-11-29 10:45:54 +0700

    Update HeroSection.vue styling

--- components/landing/HeroSection.vue
+++ components/landing/HeroSection.vue
@@ -9,7 +9,7 @@
           <span class="bg-primary">Inovasi</span> baru
         </h2>
         <div
-          class="py-8 w-[550px] text-green-950 text-lg font-medium font-raleway leading-loose text-justify"
+          class="py-8 w-[500px] text-green-950 text-lg font-medium font-raleway leading-loose text-justify"
         >
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, a
@@ -42,16 +42,11 @@
         </div>
       </div>
       <div class="flex flex-row">
-        <div class="relative">
-          <div class="z-10">
-            <NuxtImg
-              src="https://raw.githubusercontent.com/rstubryan/assets/master/gambar/lawaklu/ketawa.png"
-              class="rounded-full w-96 h-96 bg-primary"
-            />
-          </div>
-          <div
-            class="absolute top-0 left-0 w-96 h-96 rounded-full bg-gradient-to-l from-primary opacity-60 flex flex-row ml-20 hover:opacity-0 transition duration-500 ease-in-out"
-          ></div>
+        <div class="z-10">
+          <NuxtImg
+            src="https://raw.githubusercontent.com/rstubryan/assets/master/gambar/lawaklu/ketawa.png"
+            class="rounded-full w-96 h-96 bg-primary"
+          />
         </div>
       </div>
       <div class="flex flex-row mx-auto">
