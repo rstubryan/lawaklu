d585c45330058f4e32ca875d6dafd68af69ae83c
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-12-02 14:48:39 +0700

    Refactor SpacingBar component styles

--- components/landing/SpacingBar.vue
+++ components/landing/SpacingBar.vue
@@ -2,11 +2,13 @@
   <div>
     <section id="spacing">
       <div class="py-4">
-        <div class="bg-green-950 w-full h-72 font-raleway">
+        <div class="bg-green-950 md:w-full h-72 font-raleway">
           <div class="container mx-auto">
-            <div class="md:flex flex-row justify-between items-center">
+            <div class="md:flex md:flex-row md:justify-between md:items-center">
               <div class="w-96">
-                <h2 class="md:text-4xl font-bold text-white">
+                <h2
+                  class="md:text-4xl text-2xl font-bold text-white md:p-0 p-8 md:text-start text-center"
+                >
                   lorem ipsum dolor sit amet consectetur adipisicing elit
                 </h2>
               </div>
@@ -16,7 +18,9 @@
                   class="py-0 h-72"
                 />
               </div>
-              <div class="md:flex flex-row gap-32 mr-6 text-white">
+              <div
+                class="md:flex flex-row gap-32 mr-6 text-white md:p-0 p-8 md:text-start text-center md:text-lg text-base"
+              >
                 <div>
                   <h3 class="md:text-xl font-bold">lorem ipsum</h3>
                   <p class="">lorem</p>
